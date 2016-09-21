abd6942102cd19f51769f27e58eb28a83b7e2e34
Kristina Chang <kristinachang@kristinhangsmbp.lan>
2016-09-20 21:46:00 -0700

    create module myTitle.js, create public folder, generate bundle for build, global requires, create build script in package.json, script src in index.html.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,12 @@
+  /* global React ReactDOM require */
+  var React = require('react')
+  var ReactDOM = require('react-dom')
   var div = React.DOM.div
   var h1 = React.DOM.h1
 
   // What if I want my own functional component? React allows me to create my own through .createClass
   // createClass needs render method, and all it does is return whatever.
-  var MyTitle = React.createClass({
-  	// same as render: function() {...}
-  	render () {
-  		return ( // use () to return multi-line.
-  			// div(null, ...) this null is used to add attributes.
-  			// this null can be: div({style:{color:'red'}}, ...)
-  			div(null,
-  				// h1(null, "Hello my Title")
-  				// how do you use Class? instantiate it. can use keyword new, but React helps instantiate, see note in MyFirstComponent.
-  				// h1(null, this.props.title) // h1({style:{color:'red'}}, "Hello React!!")
-  				h1({style: {color: this.props.color}}, this.props.title)
-  			)
-  		)
-  	}
-  })
+  var MyTitle = require('./MyTitle')
 
   var TitleFact = React.createFactory(MyTitle) // create a Factory and pass in the class name.
   var ce = React.createElement // here's another way.
